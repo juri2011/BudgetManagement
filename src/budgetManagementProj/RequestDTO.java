65da5ed2050db0eb39a707e8e63f2f952fb65c0e
이하나 <lubihana12@gmail.com>
2024-01-23 19:21:56 +0900

    DTO초안 작성

--- src/budgetManagementProj/RequestDTO.java
+++ src/budgetManagementProj/RequestDTO.java
@@ -1,5 +1,24 @@
 package budgetManagementProj;
 
+import java.util.Date;
+
 public class RequestDTO {
-//저장될 데이터: 잔액(int), 수입(int), 지출수입(int), 기입날짜(Date), 항목(String)
+  
+  //사용자로부터 전달받은 데이터 저장클래스 
+  //저장될 데이터: 잔액(int), 수입(int), 지출수입(int), 기입날짜(Date), 항목(String)
+  private int balance;
+  private int income;
+  private int expense;
+  private Date date;
+  private String desc;
+  
+  //생성자 함수 작성.
+  public RequestDTO(int balance, int income, int expense, Date date, String desc) {
+    this.balance = balance;
+    this.income = income ;
+    this.expense = expense;
+    this.date = date;
+    this.desc = desc ;
+    
+  }
 }
