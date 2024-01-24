a09da4f835865ada7f20c592894bc91b621f6ed1
이하나 <lubihana12@gmail.com>
2024-01-24 12:03:39 +0900

    DAO 변수병 변경 및 추가

--- src/budgetManagementProj/BudgetDAO.java
+++ src/budgetManagementProj/BudgetDAO.java
@@ -6,13 +6,18 @@ import java.util.Map;
 
 public class BudgetDAO {
   //저장될 데이터: 잔액(int), 수입(int), 지출수입(int), 기입날짜(Date), 항목(String)
-  //데이터베이스 저장소 역할을 대행하는 Map 객체
-  private static int balance = 0;
+  //데이터베이스 저장소 역할을 대행하는 Map 객체  
+  private static int totBalance = 0;
   private static Map<Integer, BudgetVO> budMap = new HashMap<Integer, BudgetVO>();
   
   //MemberVO 타입의 데이터를 받아 DB에 저장하는 함수
   public void insert(BudgetVO vo) {
     budMap.put(vo.getContentNum(), vo);
+  }
+  
+  //MemberVO 의 총잔액을 받아와서 리턴하는 함수
+  public int getTotBalance() {
+    return totBalance;
   }
   
   //전달받은 항목 
@@ -29,5 +34,8 @@ public class BudgetDAO {
   public void update(BudgetVO vo) {
     budMap.put(vo.getContentNum(), vo);
   }
+  
+  
+
   
 }
