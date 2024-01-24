70cbf1807856ca2ee95d83e7d8297fceaa0414c4
이하나 <lubihana12@gmail.com>
2024-01-24 11:50:12 +0900

    DTO 구조변경했습니다.

--- src/budgetManagementProj/RequestDTO.java
+++ src/budgetManagementProj/RequestDTO.java
@@ -6,19 +6,21 @@ public class RequestDTO {
   
   //사용자로부터 전달받은 데이터 저장클래스 
   //저장될 데이터: 잔액(int), 수입(int), 지출수입(int), 기입날짜(Date), 항목(String)
+  private int contentNum;
   private int balance;
-  private int income;
-  private int expense;
-  private Date date;
   private String desc;
+  private int money;
+  private Date adddate;
+  private String memo;
+
+  
   
   //생성자 함수 작성.
-  public RequestDTO(int balance, int income, int expense, Date date, String desc) {
-    this.balance = balance;
-    this.income = income ;
-    this.expense = expense;
-    this.date = date;
-    this.desc = desc ;
-    
+  public RequestDTO(int contentNum, String desc, int money,String memo, Date addDate) {
+    this.contentNum = contentNum;
+    this.desc = desc;
+    this.money = money;
+    this.memo = memo;
+    this.adddate = addDate;    
   }
 }
